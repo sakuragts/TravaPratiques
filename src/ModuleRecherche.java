f17af69621a3ebf9d1246e12f944077c37a912d4
sakuragts <BoboboPuniPotato44>
2019-03-07 13:09:53 -0500

    added MSG_DEBUT

--- src/ModuleRecherche.java
+++ src/ModuleRecherche.java
@@ -1,2 +1,10 @@
 public class ModuleRecherche {
+    public final String MSG_DEBUT = "Cette application permet d'executer diverses requetes pour rechercher des livres\n" +
+            "dans une bibliotheque donnee. Elle permet plus particulierement de faire des\n" +
+            "recherches par categorie(s), par expression dans le titre, par auteur, et par\n" +
+            "periode de publication.";
+
+    public static void afficherMenu(){
+
+    }
 }
