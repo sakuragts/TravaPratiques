beb4dea34e88bd48bd2798d72aaf762ba5a84f59
sakuragts <sakura_gts@hotmail.com>
2019-03-09 16:23:43 -0500

    did methods to search for options in text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        String biblio = UtilitaireTP2.lireBibliotheque();
+        String test = "policier";
 
-        System.out.println(ModuleRecherche.findCategorie());
+        System.out.println(ModuleRecherche.recherche(biblio, "policier"));
+
     }
+
 }
--- src/ModuleRecherche.java
+++ src/ModuleRecherche.java
@@ -5,6 +5,8 @@ public class ModuleRecherche {
             "periode de publication.";
     public static final String ERR_CHOIX = "Erreur, choix invalide! Recommencez...";
     public static final String MSG_FIN = "FIN DU PROGRAMME. AU REVOIR!";
+    public static final String ERR_PAS_TROUVE = "AUCUN LIVRE TROUVE.";
+    public static final String MSG_ENTREE = "Tapez <ENTREE> pour revenir au menu de recherche... ";
 
     //affiche le menu principal
     public static void afficherMenu(){
@@ -17,7 +19,7 @@ public class ModuleRecherche {
         + "4. Recherche par periode\n"
         + "5. Quitter\n"
         + "\n"
-        + "Entrez votre choix :");
+        + "Entrez votre choix : ");
     }
 
     //prends et valide le choix de l'utilisateur
@@ -35,31 +37,49 @@ public class ModuleRecherche {
         }while(!boolChoix);
         return choix;
     }
-    //va chercher le fichier bibliotheque
-    public static String appelleBiblio(){
-        String biblio = UtilitaireTP2.lireBibliotheque();
-        return biblio;
+
+    public static boolean compareOption(String subString, String option){
+       String compareOption;
+       boolean compare = false;
+       int index;
+        for(int i = 0; i < subString.length(); i = index){
+            index = subString.indexOf('\t', i + 1);
+            if(index < 0){
+                index = subString.length();
+            }
+            compareOption = subString.substring(i, index);
+            compareOption = compareOption.trim();
+            if(compareOption.contains(option) || compareOption.compareToIgnoreCase(option) == 0){
+                compare = true;
+            }
+       }
+        return compare;
     }
 
-    public static String findCategorie(){
-        String categorie = "";
-        String biblio = appelleBiblio();
-        int index = 0;
-        int index2 = 0;
+    public static String recherche(String biblio, String option){
+        String resultatRecherche = "";
+        String rechercheSub;
+        int indexDebut;
+        int indexFin;
 
-        for(int i = 0; i < 4; i++){
-            index = biblio.indexOf('\t', index);
-            index++;
-            System.out.println(index);
+        for(int i = 0; i < biblio.length(); i = indexFin){
+            indexDebut = i;
+            indexFin = biblio.indexOf('\n', indexDebut);
+            if (indexFin >= 0) {
+                rechercheSub = biblio.substring(indexDebut, indexFin);
+                if (compareOption(rechercheSub, option)) {
+                    resultatRecherche += rechercheSub + '\n';
+                }
+                indexFin++;
+            }else{
+                indexFin = biblio.length();
+            }
+
+        }
+        if (resultatRecherche.isEmpty()){
+            System.out.println(ERR_PAS_TROUVE);
         }
 
-        System.out.println(biblio.substring(index));
-
-        return categorie;
-    }
-
-    public static String recherche(String option){
-
-        return option;
+        return resultatRecherche;
     }
 }
