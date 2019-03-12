f3c365871d2762758c0b15ab7dca43b0702210b1
sakuragts <sakura_gts@hotmail.com>
2019-03-12 01:10:31 -0400

    added formatLivre

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         String biblio = UtilitaireTP2.lireBibliotheque();
         String test = "policier";
 
-        ModuleRecherche.validerChoix("Veuillez choisir entre 1 a 4: ", "Erreur! Choix invalide",  '1', '4');
-
+        //ModuleRecherche.validerChoix("Veuillez choisir entre 1 a 4: ", "Erreur! Choix invalide",  '1', '4');
+        System.out.println(ModuleRecherche.formatLivre("Romance et de si belles fiancailles	M. H. Clark	2018	thriller	policier	Romance"));
     }
 
 }
--- src/ModuleRecherche.java
+++ src/ModuleRecherche.java
@@ -25,37 +25,10 @@ public class ModuleRecherche {
     public static final String MSG_ENTREE = "Tapez <ENTREE> pour revenir au menu de rechercheEntree... ";
 
 
-    //affiche un menu et valide le choix de l'utilisateur
-    /*public static String valideEntreeString(String msgMenu, String msgErr, String min, String max) {
-        String entree;
-        int intEntree;
-        int intMin;
-        int intMax;
-        boolean estInvalide = true;
 
-        do {
-            System.out.print(msgMenu);
-            entree = Clavier.lireString();
-            if (entree != null && !entree.isEmpty()) {
-                if(entree.length() < 1 && entree.charAt(0) > '1' && entree.charAt(0) < '9') {
-                    intEntree = Integer.parseInt(entree);
-                    intMin = Integer.parseInt(min);
-                    intMax = Integer.parseInt(max);
-                    if (intEntree < intMin && intEntree > intMax) {
-                        System.out.println(msgErr);
-                    } else {
-                        estInvalide = false;
-                    }
-                }
-            }else{
-                System.out.println(msgErr);
-            }
-        }while(estInvalide);
-        return entree;
-    }*/
 
     //prends et valide le choix de l'utilisateur
-   public static String validerChoix(String msgMenu, String msgErr, char min, char max){
+   public static String validerChoix(String msgMenu, String msgErr, char min, char max) {
         boolean boolChoix;
         String choix;
         do{
@@ -66,7 +39,7 @@ public class ModuleRecherche {
                 System.out.println(msgErr);
                 boolChoix = false;
             }else if (choix == null ||
-                    choix.charAt(0) < min || choix.charAt(0) > max){
+                    choix.charAt(0) < min || choix.charAt(0) > max) {
                 System.out.println(msgErr);
                 boolChoix = false;
             }
@@ -74,21 +47,51 @@ public class ModuleRecherche {
         return choix;
     }
 
+    /*Prends en parametre une ligne du resultat de la recherche et le
+    formatte selon la convention donnee*/
+    public static String formatLivre(String resultatSubString) {
+        int indiceTab = 0;
+        int indexTab = 0;
+        String formateSubString = "";
 
-    /*Prends en parametre une ligne du document de references et l'entree recherchee. L'entree recherchee est comparee
-    a chaque bloc separe par un espace tab pour trouver s'il est contenu dans la ligne visee*/
+        indexTab = resultatSubString.indexOf('\t');
+
+        for(int i = indexTab; i < resultatSubString.length(); i = indexTab) {
+            indexTab = resultatSubString.indexOf('\t', i + 1);
+            indiceTab++;
+            if(indexTab < 0) {
+                indexTab = resultatSubString.length();
+            }
+
+            if(indiceTab == 2) {
+                formateSubString = resultatSubString.substring(0, i).toUpperCase() + " (";
+            }else if(indiceTab == 3) {
+                formateSubString += resultatSubString.substring(i - 4, i) + "), [ " +
+                        resultatSubString.substring(i + 1).replace('\t', ',').toLowerCase();
+            }
+
+        }
+        formateSubString = "- " +formateSubString + " ]";
+
+        return formateSubString;
+    }
+
+
+    /*Prends en parametre une ligne du document de references et l'entree recherchee. L'entree
+    recherchee est comparee a chaque bloc separe par un espace tab pour trouver s'il est
+    contenu dans la ligne visee*/
     public static boolean compareOption(String subString, String entree) {
         String compareOption;
         boolean compare = false;
         int index;
-        for(int i = 0; i < subString.length(); i = index){
+        for(int i = 0; i < subString.length(); i = index) {
             index = subString.indexOf('\t', i + 1);
             if(index < 0){
                 index = subString.length();
             }
             compareOption = subString.substring(i, index);
             compareOption = compareOption.trim();
-            if(compareOption.contains(entree) || compareOption.compareToIgnoreCase(entree) == 0){
+            if(compareOption.contains(entree) || compareOption.compareToIgnoreCase(entree) == 0) {
                 compare = true;
             }
         }
@@ -112,7 +115,7 @@ public class ModuleRecherche {
                     resultatRecherche += rechercheSub + '\n';
                 }
                 indexFin++;
-            }else{
+            }else {
                 indexFin = biblio.length();
             }
 
