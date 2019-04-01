89e0ef9bc5aef6c2ea877701fed9b3624aabc417
sakuragts <BoboboPuniPotato44>
2019-04-01 13:15:12 -0400

    added empty constructor and 2 methods

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -11,4 +11,33 @@
  */
 
 public class Bibliotheque {
+
+    public final static int NBR_CASES = 4;
+
+    public Livre[] livres = new Livre[NBR_CASES];
+    public int nbrLivres = 0;
+
+    public Bibliotheque() {
+        Livre[] biblio = this.livres;
+    }
+
+    public boolean livreExiste(Livre[] biblio, Livre livre) {
+        boolean livreExiste = false;
+        int indexBiblio = 0;
+
+        while (!livreExiste && indexBiblio <= biblio.length ) {
+            if (biblio[indexBiblio].estEgal(livre)) {
+                livreExiste = true;
+            }
+            indexBiblio++;
+        }
+
+        return livreExiste;
+    }
+
+    public boolean ajouterLivre(Livre livre) {
+        boolean livreAjouter = false;
+
+        return livreAjouter;
+    }
 }
